c3bec724c161debc82086ee06fa7cbe6f98c824d
rajakumargupta <alliswell14395@gmail.com>
2017-02-11 14:30:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+var img=document.getElementById('img');
+var marginLeft=0;
+function moveRight()
+{
+    
+    marginLeft=marginLeft+1;
+    img.style.marginLeft+'px';
+    }
+    img.onclick=function()
+    {
+        var interval=setInterval(moveRight,50);
+    };
